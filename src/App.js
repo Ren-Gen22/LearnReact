2b8892bfcd868648af293cc80daa0323460aee9e
Ren-Gen22 <abhishekrenjan33@gmail.com>
2023-11-24 22:24:37 +0530

    hooks clicker

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@
 import './App.css';
 import {useState,useEffect} from 'react';
 
-const App = () => {
-  const [counter,setCounter] =useState(0);
+function Example() {
 
-  useEffect(()=>{
-    alert('changed'+counter)
-  },[counter]);
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <button onClick={()=> setCounter((prevCount)=>prevCount-1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={()=> setCounter((prevCount)=>prevCount+1)}>+</button>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
+}
+
+const App = () => {
+
+  return (
+    Example()  );
 };
 
 export default App;
